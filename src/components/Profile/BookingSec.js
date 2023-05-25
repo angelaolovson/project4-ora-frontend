6f917a8ae890c040e956ddd48bae73b2d2ccafcd
Angela Olovson <angela.olovson@gmail.com>
2023-05-24 22:11:41 -0400

    Finished edit booking

--- src/components/Profile/BookingSec.js
+++ src/components/Profile/BookingSec.js
@@ -1,64 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import './Profile.css';
 import Figure from 'react-bootstrap/Figure';
+import EditBooking from './EditBooking';
+
 
 const BookingSec = ({ booking }) => {
 
-  //filter the past booking
-  const currentDate= new Date();
-    const futureBooking = booking.filter(booking => {
-      const bookingDate = new Date(booking.startDate);
-      return bookingDate >= currentDate;
-    });
+  const [AddModalState,setAddModal] = useState(null)
+  const handleAddModalClose = ()=>setAddModal(null);
+  const handleAddModalOpen = (index)=>setAddModal(index);
+
+   //filter the past booking
+   const currentDate= new Date();
+   const futureBooking = booking.filter(booking => {
+     const bookingDate = new Date(booking.startDate);
+     return bookingDate >= currentDate;
+   });
 
 console.log(futureBooking)
 
-  const property = futureBooking.map((info, index) => {
-    const startDate = new Date(info.startDate).toLocaleDateString(undefined,{month:'short', day: 'numeric', year:'numeric'});
-    const endDate = new Date(info.endDate).toLocaleDateString(undefined,{month:'short', day: 'numeric', year:'numeric'});
-    const createDate = new Date(info.createdAt).toLocaleDateString()
-    return (
-      <Card key={index} className='booking'>
-        <Card.Body>
-        <Row>
-          <Col >
-          <Figure>
-              <Figure.Image
-                className='profile-image'
-                width={250}
-                height={400}
-                alt="171x180"
-                src={info.image}
-              />
-            </Figure>
-          </Col>
-          <Col className='booking-col'>
-          <NavLink to={`/listing/${info.listing}`}>
-        <Card.Title as="h5">{info.city}</Card.Title>
-        </NavLink>
-          <Card.Title> {startDate} to {endDate}</Card.Title>
-           <Card.Text className='text-left'>
-            {info.address}
-           </Card.Text>
-          <Card.Text>
-          Total {info.totalPrice} USD 
+ const bookingsList = futureBooking.map((info, index) => {
+   const startDate = new Date(info.startDate).toLocaleDateString(undefined,{month:'short', day: 'numeric', year:'numeric'});
+   const endDate = new Date(info.endDate).toLocaleDateString(undefined,{month:'short', day: 'numeric', year:'numeric'});
+   const createDate = new Date(info.createdAt).toLocaleDateString()
+   return (
+     <Card key={index} className='booking'>
+       <Card.Body>
+       <Row>
+         <Col >
+         <Figure>
+             <Figure.Image
+               className='profile-image'
+               width={250}
+               height={400}
+               alt="171x180"
+               src={info.image}
+             />
+           </Figure>
+         </Col>
+         <Col className='booking-col'>
+         <NavLink to={`/listing/${info.listing}`}>
+       <Card.Title as="h5">{info.city}</Card.Title>
+       </NavLink>
+         <Card.Title> {startDate} to {endDate}</Card.Title>
+          <Card.Text className='text-left'>
+           {info.address}
           </Card.Text>
-          <Button variant="outline-secondary">Reschedule or Cancel</Button>
-          </Col>
-          </Row>
-          <p>Created at {createDate}</p>
-        </Card.Body>
-  </Card>
-    )
-});
-   // No booking
-   if (!futureBooking || futureBooking.length === 0) {
-    return <p>No Future Booking Coming</p>;
-  }
+         <Card.Text>
+         Total ${info.totalPrice} USD 
+         </Card.Text>
 
-  return <Row className="hostListing">{property}</Row>;
+
+         <Button 
+          variant="outline-secondary"
+          show = {AddModalState === index ? "true" : "false"}
+          onClick={() => handleAddModalOpen(index)}
+          >Reschedule or Cancel</Button>
+          {AddModalState === index && 
+          <EditBooking
+          show={true}
+          handleClose={handleAddModalClose}
+          bookingData={info}/>
+          }
+         
+         </Col>
+         </Row>
+         <p>Created at {createDate}</p>
+       </Card.Body>
+ </Card>
+   )
+});
+  // No booking
+  if (!futureBooking || futureBooking.length === 0) {
+   return <p>No Future Booking Coming</p>;
+ }
+
+ return <Row className="hostListing">{bookingsList}</Row>;
 };
 
 export default BookingSec;
--- src/components/Profile/EditBooking.js
+++ src/components/Profile/EditBooking.js
@@ -1,8 +1,147 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
-const EditBooking = () => {
+const EditBooking = ({show,handleClose,bookingData}) => {
+  // Edit Booking State
+  const [startDate, setStartDate] = useState(`${bookingData.startDate.substr(0,10)}`);
+  const [endDate, setEndDate] = useState(`${bookingData.endDate.substr(0,10)}`);
+  const [getPrice, setGetPrice] = useState(null);
+
+  //get price from the property
+  const url = `https://airbnb-main.onrender.com/listing/${bookingData.listing}`;
+  
+  useEffect(() => {
+    const fetchThisProperty = async () => {
+      // console.log("going to fetch property with id of: ", id);
+      try {
+        const responseData = await fetch(url);
+        const thisPropertyData = await responseData.json();
+        console.log(thisPropertyData, thisPropertyData.property.price, "get priceeeee")
+        setGetPrice(thisPropertyData.property.price)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    fetchThisProperty();
+    
+  }, [url])
+  
+  //onChange handler
+  const onChangeHandler = (e, setValue) => {
+    console.log(e.target.value);
+    setValue(e.target.value); //this represents any state updater (setName) that we passed in
+  }; 
+
+  // onSubmit
+  const onSubmitHandler = async (e) => {
+		e.preventDefault();
+    
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const days = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
+		const serviceFee = parseFloat((getPrice * days * 0.04).toFixed(2));
+	  const tax = parseFloat((getPrice * days * 0.06).toFixed(2));
+		const finalTotalPrice = parseFloat((getPrice * days + serviceFee + tax).toFixed(2));
+
+    // Perform booking submission logic here
+    const updatedBooking = {
+      guest: bookingData.guest,
+      listing: bookingData.listing,
+			startDate: startDate,
+			endDate: endDate,
+      address: bookingData.address,
+      image: bookingData.image,
+      city: bookingData.city,
+      listing: bookingData.listing,
+			totalPrice: finalTotalPrice
+    }
+    console.log("updated booking",updatedBooking)
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedBooking)
+    }
+    const responseData = await fetch(`https://airbnb-main.onrender.com/booking/${bookingData._id}` , options)
+    const updatedBookingObj = await responseData.json()
+    console.log(updatedBookingObj)
+    handleClose();
+    window.location.reload();
+  }  //-------------onSubmit end
+
+  //--------onDeleteHandler
+  const onDeleteHandler = async (event) => {
+    event.preventDefault();
+    console.log("Delete review with ", bookingData._id)
+
+    const options = {
+      method: "DELETE",
+    };
+
+    const responseData = await fetch(`https://airbnb-main.onrender.com/booking/${bookingData._id}`, options);
+
+    const response = await responseData.json();
+    console.log(response);
+    window.location.reload();
+  };
+
+
+  const start = new Date(startDate);
+	const end = new Date(endDate);
+	const days = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
+	const serviceFee = parseFloat((getPrice * days * 0.04).toFixed(2));
+	const tax = parseFloat((getPrice * days * 0.06).toFixed(2));
+	const finalTotalPrice = parseFloat((getPrice * days + serviceFee + tax).toFixed(2));
+
   return (
-    <div>EditBooking</div>
+    <div>
+      <Modal show={show} onHide={handleClose} className='modal-background'>
+
+        <Modal.Header closeButton onClick={()=>handleClose()}>
+          <Modal.Title>Update Booking</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form onSubmit ={onSubmitHandler}>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Start Date</Form.Label>
+              <Form.Control
+                type="date"
+                autoFocus
+                value = {startDate}
+                onChange ={(e) => onChangeHandler(e,setStartDate)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>End Date</Form.Label>
+              <Form.Control
+                type="date"
+                autoFocus
+                value = {endDate}
+                onChange ={(e) => onChangeHandler(e,setEndDate)}
+              />
+            </Form.Group>
+
+            <Form.Label>Service Fee $ {serviceFee} </Form.Label>
+            <Form.Label>Tax $ {tax} </Form.Label>
+            <Form.Label>New Total $ {finalTotalPrice} </Form.Label>
+
+            <Button variant="warning" size="lg" type='submit'>Update Booking</Button>
+
+            <Button variant="danger" size="lg" type='submit' onClick ={onDeleteHandler}>Cancel Booking</Button>  
+
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer></Modal.Footer>
+
+      </Modal>
+    </div>
   )
 }
 
