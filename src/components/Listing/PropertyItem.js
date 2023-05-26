0eaaaa00e0849e6f8beac87742dd7f550ff165dc
Angela Olovson <angela.olovson@gmail.com>
2023-05-26 13:38:33 -0400

    Styled listing page

--- src/components/Listing/PropertyItem.js
+++ src/components/Listing/PropertyItem.js
@@ -5,19 +5,29 @@ import { Card } from 'react-bootstrap'
 
 	  
 function PropertyItem(props) {
-	const {property} = props
+	const {property} = props;
+	const solidStar = '\u2605';
 
     return (
 		
-		<NavLink to={`/listing/${property._id}`}>
+		<NavLink to={`/listing/${property._id}`} className="noUnderLine">
 			<Card className="propertyitem" style={{ border: 'none' }}>
 				<div className='imgContainer'>
 					<Card.Img className='itemImg' variant="top" src={property.images[0]} />
 				</div>
-				<Card.Body className='cardBody'>
-					<Card.Text className='cardTitle'>{property.city}, {property.country}</Card.Text>
-					<Card.Text className='cardText'>{property.title}</Card.Text>
-					<Card.Text className='cardText'>${property.price} night</Card.Text>
+				<Card.Body className='propertyitemCardBody'>
+					
+					<div className='inlineLocationRating'>
+						<Card.Text className='propertyitemLocation'>{property.city}, {property.country}</Card.Text>
+						<Card.Text className='propertyitemRating'>{solidStar}{parseFloat((property.rating).toFixed(2))}</Card.Text>
+					</div>
+					
+					<Card.Text className='propertyitemTitle'>{property.title}</Card.Text>
+
+					<div className='inlinePrice'>
+						<Card.Text className='propertyitemPriceNumber'>${property.price}</Card.Text>
+						<Card.Text className='propertyitemPriceText'>night</Card.Text>
+					</div>
 				</Card.Body>
 			</Card>
 		</NavLink>		  
