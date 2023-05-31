cfd2d880d2314663a7636467e1615a5c12942c18
Angela Olovson <angela.olovson@gmail.com>
2023-05-30 23:01:22 -0400

    Complated HostInfo

--- src/components/Listing/HostInfo.js
+++ src/components/Listing/HostInfo.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './HostInfo.css'
 import '../../pages/Host'
-import Host from '../../pages/Host'
+import { NavLink } from 'react-router-dom'
 function HostInfo({host}) {
+	const solidStar = '\u2605';
     return (
-		<div className="host">
-			<h2>Host: {host.username} </h2>
-			<Host />
-			
+		<div className="hostInfo">
+			<div className='ImgAndDescription'>
+				<div className="hostImgContainer"><img className="hostImg" src={host.image} alt="hostImg"/></div>
+				<div className='hostDescriptionAndJob' >
+					<div className='job' > {solidStar} {host.reviewsGiven.length} Reviews</div>
+					<div className='job' >Occupation: {host.occupation} </div>
+					<div className='description'>Description: {host.description} </div>
+					<div className='description'>Learn more about <NavLink to = {`/user/${host._id}`} >{host.username}</NavLink> </div>
+				</div>
+			</div>
 		</div>
 	)
 }
