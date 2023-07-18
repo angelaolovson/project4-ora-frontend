3ba801a24520fabe58c3de74ce772ae6849eab59
Angela Olovson <angela.olovson@gmail.com>
2023-07-17 20:53:05 -0400

    Styled Checkout

--- src/components/CheckOut/CustomerInfo.js
+++ src/components/CheckOut/CustomerInfo.js
@@ -1,10 +1,68 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CustomerInfo.css'
-		  
+import { AuthContext } from '../../context/auth-context';
+import { Button, Container, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+
 function CustomerInfo() {
+	const auth = useContext(AuthContext)
+  	console.log(auth.userId)
+	const navigate = useNavigate();
+
+	//states handler
+	const onChangeHandler = (e,setValue) => {
+		setValue(e.target.value);
+	}
+	
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const newOrder = {
+			
+		}
+		//console.log('new listing',newOrder);
+		
+		try{   
+			const options = {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newOrder),
+			};
+
+			const responseData = await fetch(
+				"http://localhost:4000/order", options
+			)
+			const newOrderObj = await responseData.json();
+			console.log(newOrderObj)
+			
+			navigate('/')
+		} catch (error){
+			console.log(error)
+		}
+	};
+
     return (
 		<div className="customerinfo">
 			<div>CheckOut Info name, address, blablabla</div>
+			<Container >
+			{/* <Form onSubmit={handleSubmit}>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label style ={{marginTop:'70px'}}>Title</Form.Label>
+					<Form.Control 
+						type="text" 
+						value ={titleState} 
+						placeholder="Product Name" 
+						onChange={(e) => onChangeHandler(e, setTitleState)}
+						required/>
+				</Form.Group>
+
+				<Button variant="primary" type="submit">
+					Place Order
+				</Button>
+			</Form> */}
+		</Container>
 					  
 		</div>
 	)
--- src/components/CheckOut/TotalSummary.js
+++ src/components/CheckOut/TotalSummary.js
@@ -4,22 +4,53 @@ import { CartContext } from '../../context/CartContext';
 
   
 function TotalSummary() {
-	const [cartState, setCartState] = useContext(CartContext);
-	// setCartState();
-	console.log(cartState)
+    const [cartState, setCartState] = useContext(CartContext);
+    // setCartState();
+    console.log(cartState)
+
+	// Number of items
+    const numItems = cartState ? cartState.items.length : 0;
+
+	// Subtotal
+    const subtotal = cartState 
+        ? cartState.items.reduce((sum, item) => sum + item.product.price * item.quantity, 0)
+        : 0;
+
+	// Total
+	const finalPrice = subtotal*1.088
 
     return (
-		<div className="totalsummary">
-			{cartState? (
-				<h1>{cartState.items[cartState.items.length-1].product.title} </h1>
-			) : 
-			<div>loading</div>
-			}
-			
-					  
-		</div>
-	)
+        <div className="totalsummary">
+			<div className="totalsummaryTitle">Order Summary</div>
+			<div className="totalsummaryItemSub">
+				<div className="totalsummaryItems">{numItems} items</div>
+                <div className="totalsummarySub">Subtotal: ${subtotal.toFixed(2)}</div>
+			</div>
+            {cartState ? (
+                <>
+                    {cartState.items.map((item, index) => (
+						<div className='totalsummaryContainer' key={index}>
+							<div className='totalsummaryImgContainer'>
+								<img className='totalsummaryImg' src={item.product.images[0]} alt={item.product.title} />
+							</div>
+							<div className='totalsummaryItemInfo'>
+								<div className='totalsummaryItemName'>{item.product.title}</div>
+								<div className='totalsummaryItemqty'>QTY:{item.quantity}</div>
+								<div className='totalsummaryItemprice'>${(item.product.price*item.quantity).toFixed(2)}</div>
+							</div>
+						</div>
+					))}
+                </>
+				
+            )
+			 : 
+            <div>Loading...</div>
+            }	
+			<div>total: ${finalPrice.toFixed(2)}</div>		  
+        </div>
+    )
 }
-		  
+
 export default TotalSummary
+
 		  
